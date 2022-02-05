d00bceefe7c87abeb7ec55cdd4405a36f84c0598
tecniphone <quim.tecniphone@gmail.pt>
2022-02-05 18:10:23 +0000

    criado

---
+++ src/testeed/TesteEd.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testeed;
+
+/**
+ *
+ * @author Utilizador
+ */
+public class TesteEd {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
